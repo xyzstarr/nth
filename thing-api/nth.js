0dd85825158de0e97b71288be8b877ccf4b8aae8
Thitos <thatomantai@gmail.com>
2016-03-22 18:19:49 +0200

    .

---
+++ thing-api/nth.js
@@ -0,0 +1,11 @@
+module.exports = nth
+
+function nth() {
+	console.log('God Is Great');	
+	this.InTheBeginning = in_the_beginning    
+	function in_the_beginning(_nth){
+		console.log('In The Beginning Was The Word\n' + _nth)
+	}
+	
+		return this
+}
